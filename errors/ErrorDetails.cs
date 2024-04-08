551a5861d88be5b6b5c5db9389c5450e20cb19d8
afruitindisguise <156379697+afruitindisguise@users.noreply.github.com>
2024-04-08 13:04:45 -0400

    Delete errors directory

--- errors/ErrorDetails.cs
+++
@@ -1,7 +0,0 @@
-namespace Major_Project.errors{
-    public class ErrorDetails{
-        public int StatusCode { get; set; }
-        public string? Message { get; set; }
-        public string? ExceptionMessage { get; set; }
-    }
-}
