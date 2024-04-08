8415717653a8e9d4f961e67a7995d1e23252c597
afruitindisguise <156379697+afruitindisguise@users.noreply.github.com>
2024-04-08 13:04:52 -0400

    Delete models directory

--- models/Items.cs
+++
@@ -1,8 +0,0 @@
-namespace Major_Project.models{
-    public class Item{
-        public int Id { get; set; }
-        public string ItemName { get; set; } = null!;
-        public string Description { get; set; } = null!;
-        public List<Player> players { get; set; }
-    }
-}
